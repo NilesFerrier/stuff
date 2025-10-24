d28b1c9205258b2dae6c1ced8cac76670e6a4b3d
NilesFerrier <57294526+NilesFerrier@users.noreply.github.com>
2025-10-24 15:19:41 -0400

    Add files via upload

---
+++ disable_and_protect.ps1
@@ -0,0 +1,11 @@
+Get-ADUser -Filter 'Name -like "roar*"' -SearchBase "$((Get-ADDomain).DistinguishedName)" | Disable-ADAccount
+Get-ADUser -Filter 'Name -like "backup*"' -SearchBase "$((Get-ADDomain).DistinguishedName)" | Disable-ADAccount
+Get-ADUser -Filter 'Name -like "Lion*"' -SearchBase "$((Get-ADDomain).DistinguishedName)" | Disable-ADAccount
+Get-ADUser -Filter 'Name -like "ATO Roar*"' -SearchBase "$((Get-ADDomain).DistinguishedName)" | Disable-ADAccount
+
+Add-ADGroupMember -Identity "Protected Users" -Members "Atlantic"
+Add-ADGroupMember -Identity "Protected Users" -Members "abpadmin"
+Add-ADGroupMember -Identity "Protected Users" -Members "Administrator"
+Add-ADGroupMember -Identity "Protected Users" -Members "atoadmin"
+Add-ADGroupMember -Identity "Protected Users" -Members "acptech"
+Add-ADGroupMember -Identity "Protected Users" -Members "acpautomate"
