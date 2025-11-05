43c2aa0c6c03ed3b4bfc0e2d66ab4b4538458019
NilesFerrier <57294526+NilesFerrier@users.noreply.github.com>
2025-11-05 13:13:05 -0500

    Add files via upload

---
+++ gss-aella.ps1
@@ -0,0 +1,4 @@
+#General Sullivan
+#Aella Agent install
+
+msiexec /i \\gensullivan.local\netlogon\Stellar Cyber\aellads_6.1.0_windows-x64_20250902_175e21d.msi /quiet /norestart CM_HOST=cm-cyflare.stellarcyber.cloud TENANT_ID=cd825a7f79044cfaa844b7414bc4c65c AGG_HOST1="" AGG_HOST2="" 
