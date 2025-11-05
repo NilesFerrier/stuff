df079408cefd144ae46a2d8e1ab338f016d9300a
NilesFerrier <57294526+NilesFerrier@users.noreply.github.com>
2025-11-05 11:48:58 -0500

    Add files via upload

---
+++ diamond_aella.ps1
@@ -0,0 +1,5 @@
+#Diamond Communications
+#Aella Agent Install
+
+msiexec /i \\diamondcomm.local\netlogon\StellarCyber\aellads_6.1.0_windows-x64_20250902_175e21d.msi /quiet /norestart CM_HOST=cm-cyflare.stellarcyber.cloud TENANT_ID=4969d24d00b94679b5dfaf3314d0ced0 AGG_HOST1="" AGG_HOST2="" 
+
