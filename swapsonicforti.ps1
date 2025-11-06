7247730cd490d01762dbf77ba9b6b4ab4aadb39f
NilesFerrier <57294526+NilesFerrier@users.noreply.github.com>
2025-11-06 16:47:03 -0500

    Add files via upload

--- swapsonicforti.ps1
+++ swapsonicforti.ps1
@@ -6,7 +6,7 @@
 Get-WmiObject Win32_Product | Where-Object {$_.Name -Like "Sonicwall Net*"}
 
 #Remove Sonicwall Extender
-Get-WmiObject Win32_Product | Where-Object {$_.Name -Like "ProgramName*"} | ForEach-Object { $_.Uninstall() }
+Get-WmiObject Win32_Product | Where-Object {$_.Name -Like "Sonicwall Net*"} | ForEach-Object { $_.Uninstall() }
 
 #Install Forticlient
 invoke-webrequest -uri "https://drive.google.com/uc?export=download&id=1EZ0oe9uiujrnv_e4K2-ITo_euHJmweqZ" -outfile c:\temp\forticlient.msi
